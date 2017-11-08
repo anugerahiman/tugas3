1f12113ecec681f623c9e5e67d834685733b57fb
anugerahnooriman <anugerahnooriman71@gmail.com>
2017-11-08 16:11:01 +0700

    no.2.cpp

--- no.2.cpp
+++ no.2.cpp
@@ -8,5 +8,17 @@ double p,l,a,t,
 
 cout << "pilihan bidang : ";
 cin >> bidang;
-
+if (bidang == 'p' || bidang == 'p') {
+	cout << " menghitung luas persegi : ";
+	cout << " masukkan panjang : ";
+	cin >> p;
+	cout << "Masukkan lebarnya : ";
+	cin >> l;
+	L = p*l;
+	cout << " Luas persegi panjang : ";
+	}
+	
+	else{
+		cout<<" Maaf. anda salah memencet pilihan"<<endl;
+	}
 }
