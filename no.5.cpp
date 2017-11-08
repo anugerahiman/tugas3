01b2266baa0089bfb9b7885e382fefcab5d1b496
anugerahnooriman <anugerahnooriman71@gmail.com>
2017-11-08 16:35:03 +0700

    test.cpp

--- no.5.cpp
+++ no.5.cpp
@@ -8,4 +8,10 @@ int main (){
 	
 	cout << "Masukkan tinggi badan dalam cm : ";
 	cin >> tinggi;
+	
+	cout << "Masukkan berat badan dalam kg : ";
+	cin >> berat;
+	
+	if (berat<tinggi/2.5) {
+		cout << "Hasil : Kurus " <<endl;
 }
